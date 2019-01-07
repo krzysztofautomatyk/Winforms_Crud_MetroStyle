6a0c481ebebe2115871c1744644b24ee6ad178ce
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2019-01-07 12:25:25 +0100

    Przed Bindowaniem Danych

---
+++ WindowsFormsApp/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string ImieINazwisko { get; set; }
+        public string DataUrodzenia { get; set; }
+        public bool Plec { get; set; }
+        public string Email { get; set; }
+        public string Adres { get; set; }
+        public string Zdjecie { get; set; }
+    }
+
+}
+
+ 
